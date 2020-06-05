ad090ed6e7c04184eb17aef0ac1fd26ad92c2b76
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-06-05 10:41:30 -0600

    Handle routes with BottomNavigationBar}

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader_app/src/pages/direcciones_page.dart';
+import 'package:qr_reader_app/src/pages/mapas_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('hola'),),
+      body: _callPage(_currentIndex),
+      bottomNavigationBar: _crearBottonNavigationBar(),
     );
+  }
+
+  Widget _crearBottonNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      onTap: (index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones'),
+        )
+      ],
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
   }
 }
