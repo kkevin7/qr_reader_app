682702e554c2edd5471fa89b272820f97e26adb2
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-06-05 23:13:18 -0600

    Delete records from the UI

--- lib/src/pages/mapas_page.dart
+++ lib/src/pages/mapas_page.dart
@@ -8,27 +8,40 @@ class MapasPage extends StatelessWidget {
     return FutureBuilder(
       future: DBProvider.db.getTodosScans(),
       builder: (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
-        if(!snapshot.hasData){
+        if (!snapshot.hasData) {
           return Center(child: CircularProgressIndicator());
         }
-        
+
         final scans = snapshot.data;
 
-        if(scans.length == 0){
-          return(Center(
+        if (scans.length == 0) {
+          return (Center(
             child: Text('No hay información'),
           ));
         }
 
         return ListView.builder(
           itemCount: scans.length,
-          itemBuilder: (BuildContext context, int i) => ListTile(
-            leading: Icon(Icons.cloud_queue, color: Theme.of(context).primaryColor,),
-            title: Text(scans[i].valor),
-            trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey,),
+          itemBuilder: (BuildContext context, int i) => Dismissible(
+            key: UniqueKey(),
+            background: Container(
+              color: Colors.red,
+            ),
+            onDismissed: (direccion) async => await DBProvider.db.deleteScan(scans[i].id),
+            child: ListTile(
+              leading: Icon(
+                Icons.cloud_queue,
+                color: Theme.of(context).primaryColor,
+              ),
+              title: Text(scans[i].valor),
+              subtitle: Text('ID: ${scans[i].id}'),
+              trailing: Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.grey,
+              ),
+            ),
           ),
         );
-
       },
     );
   }
