037753cd38f5af1cce63983fa729fbb008fc6206
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-06-05 22:07:04 -0600

    Do Insert records on the database

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:qr_reader_app/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
 
 class DBProvider {
   static Database _database;
@@ -13,6 +18,39 @@ class DBProvider {
   }
 
   initDB() async {
-    
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'ScansDB.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) {},
+      onCreate: (Database db, int version) async {
+        await db.execute(
+          'CREATE TABLE Scans ('
+          'id INTEGER PRIMARY KEY,'
+          'tipo TEXT,'
+          'valor TEXT'
+          ')'
+        );
+      }
+    );
   }
+
+  //CREAR Registros
+  nuevoScanRow(ScanModel scanModel) async{
+    final db = await database;
+    final res = await db.rawInsert(
+      "INSERT INTO Scans(id, tipo, valor) "
+      "VALUES (${scanModel.id}, ${scanModel.tipo}, ${scanModel.valor})"
+    );
+    return res;
+  }
+
+  nuevoScan(ScanModel scanModel) async{
+    final db = await database;
+    final res = db.insert('Scans', scanModel.toJson());
+    return res;
+  }
+
+
 }
